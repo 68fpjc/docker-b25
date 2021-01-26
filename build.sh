7a77e562cc3761a808dd83872bb69f8fb45ffa9c
68fpjc <32516363+68fpjc@users.noreply.github.com>
2021-01-27 00:11:31 +0900

    ビルドスクリプトを修正

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-[ -z "${BUILDCMD}" ] && BUILDCMD="docker build"
-${BUILDCMD} "$(dirname $0)"/dockerfiles/b25/ --tag 68fpjc/b25:2021-01-23 \
-&& docker image tag 68fpjc/b25:2021-01-23 68fpjc/b25
-${BUILDCMD} "$(dirname $0)"/dockerfiles/bcas/ --tag 68fpjc/bcas:2021-01-23 \
-&& docker image tag 68fpjc/bcas:2021-01-23 68fpjc/bcas
+docker build --tag 68fpjc/b25:2021-01-23 --tag 68fpjc/b25:latest \
+"$(dirname $0)"/dockerfiles/b25/
+docker build --tag 68fpjc/bcas:2021-01-23 --tag 68fpjc/bcas:latest \
+"$(dirname $0)"/dockerfiles/bcas/
--- buildx.sh
+++ buildx.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-BUILDCMD="docker buildx build --platform linux/arm/v7 --load" $(dirname $0)/build.sh
+docker buildx create --name mybuilder
+
+docker buildx build --builder mybuilder --platform linux/arm/v7 \
+--output type=tar,dest=linux_arm_v7-b25-2021-01-23.tar \
+"$(dirname $0)"/dockerfiles/b25/
+docker buildx build --builder mybuilder --platform linux/arm/v7 \
+--output type=tar,dest=linux_arm_v7-bcas-2021-01-23.tar \
+"$(dirname $0)"/dockerfiles/bcas/
