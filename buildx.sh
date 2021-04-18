0c11b2a2f1d5f66571dbdf648ba299c207f21155
68fpjc <32516363+68fpjc@users.noreply.github.com>
2021-04-18 20:39:26 +0900

    ビルドスクリプトを調整

--- buildx.sh
+++ buildx.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
 
-docker buildx create --name mybuilder
+pushd "$(dirname $0)"
 
-docker buildx build --builder mybuilder --platform linux/arm/v7 \
---output type=tar,dest=linux_arm_v7-b25-2021-01-23.tar \
-"$(dirname $0)"/dockerfiles/b25/
-docker buildx build --builder mybuilder --platform linux/arm/v7 \
---output type=tar,dest=linux_arm_v7-bcas-2021-01-23.tar \
-"$(dirname $0)"/dockerfiles/bcas/
+docker buildx create --name b25
+
+# b25
+docker buildx build --builder mybuilder --platform linux/arm/v7 --load \
+--tag 68fpjc/b25:latest-armhf \
+dockerfiles/b25/
+
+# bcas
+docker buildx build --builder mybuilder --platform linux/arm/v7 --load \
+--tag 68fpjc/bcas:latest-armhf \
+dockerfiles/bcas/
+
+popd
+
